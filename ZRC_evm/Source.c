6d3ec3c988dd4b4fb948be67c0649999fdabd349
D1 <lyerhd@gmail.com>
2022-12-17 21:22:13 +0300

    Merged with Kolya Korzun

--- ZRC_evm/Source.c
+++ ZRC_evm/Source.c
@@ -1,70 +1,104 @@
+#include "stdio.h"
+#include "time.h"
 
+#define N 3
 
 typedef void(*FunctionPtr)();
 typedef signed short int16_t;
 typedef char int8_t;
 
 
-int size_of(int data_address) {
-	void* in_memory = data_address;
-	return sizeof(in_memory);
+
+inline void timeit(FunctionPtr* func, const int argcount, ...) {
+    /// <summary>
+    /// ... is char*, zero point is argcount
+    /// </summary>
+    /// <param name="func">function</param>
+    /// <param name="argcount">count of arguments</param>
+    /// <param name="">FUNCTION UNNECESSORY-ARGUMENTS</param>
+    int accum = 0;
+    int startup_ms, end_ms;
+    double time_spent = 0.0;
+
+    __asm {
+        lea esi, argcount;
+
+        //
+        ; size of stack for call func is argcount * 4
+
+            mov eax, argcount;
+        mov ebx, 4
+            mul ebx;
+        mov accum, eax;
+        add esi, eax;
+        mov ecx, argcount
+            //
+            PushArgs :
+        push[esi];
+        sub esi, 4; move back
+            loop PushArgs;
+        call clock; get start ticks
+            mov startup_ms, eax;
+        call[func];
+        add esp, accum;
+        call clock; get old ticks
+            mov end_ms, eax;
+
+    }
+    printf("END TIME IS %i", (end_ms - startup_ms));
 }
 
 
-inline void timeit(FunctionPtr* func, int argcount,...) {
-	/// <summary>
-	/// ... is char*, zero point is argcount
-	/// </summary>
-	/// <param name="func">function</param>
-	/// <param name="argcount">count of arguments</param>
-	/// <param name="">FUNCTION UNNECESSORY-ARGUMENTS</param>
-	int accum=0;
-	
-	double time_spent=0.0;
-	__asm {
-		lea esi, argcount;
-		mov ecx, argcount;
-		add esi, ecx;
-		nop;
-		AllocateSize:					; move back to argcount address and count bytes
-			push ecx					; to save current ecx
-			push esi;
-			call size_of;
-			add accum, eax				; append eax 
-			add esp, 4					; 
-			sub esi, 1					;
-			pop ecx;
-		loop AllocateSize;
-		mov ecx, argcount				; add argcount to accumulator
-		add esi, ecx					;
+void add16_reg(int size_, int16_t* a, int16_t* b) {
+    /// <summary>
+    /// add func for int16_t
+    /// </summary>
+    /// <param name="a">first member</param>
+    /// <param name="b">second member</param>
+    printf("adr in add16 1, 2: %u %u\n", a, b);
+    printf("adr in add16 1, 2: %i %i\n", a, b);
 
-		PushArgs:
-			push [esi]					;
-			sub esi, 1					; move back
-		loop PushArgs;
+    __asm {
+        mov ecx, size_;
+        mov edi, a;
+        mov esi, b;
+    Addition:
+        mov ax, [esi];
+        mov bx, [edi];
+        add ax, bx;
+        add esi, 2
+            add edi, 2
+            loop Addition;
+    }
 
-		call	[func];
-		add esp, accum;
-	}
+
 }
 
 
-inline void add16_reg(int16_t a, int16_t b) {
-	/// <summary>
-	/// add func for int16_t
-	/// </summary>
-	/// <param name="a">first member</param>
-	/// <param name="b">second member</param>
-
-	__asm {
-		mov ax, a;
-		mov bx, b;
-		add ax, bx;
-	}
+inline void add64_reg(long long a, long long b) {
+    a + b;
 }
 
 
 int main() {
-	int16_t x = 10, y = 20;
-	timeit(*add16_reg, 2, x, y);
+    srand(0);
+
+    int16_t* x, * y;
+    x = (int16_t*)malloc(N * sizeof(int16_t));
+    y = (int16_t*)malloc(N * sizeof(int16_t));
+
+    printf("adr in main  1, 2: %u %u\n", x, y);
+    printf("adr in main  1, 2: %i %i\n", x, y);
+
+    for (int i = 0; i < N; i++) {
+        x[i] = (int16_t)rand();
+        y[i] = (int16_t)rand();
+    }
+
+    timeit(add16_reg, 3, N, x, y);
+
+
+    free(x);
+    free(y);
+
 }
